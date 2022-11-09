1e9ffcf892d6071ba927ec08c1455554d5196c76
Kenta Oshima <84490675+mxvish@users.noreply.github.com>
2022-11-09 23:07:35 +0900

    Add files via upload

--- home/script.js
+++ home/script.js
@@ -4,6 +4,9 @@ function check(num, answers) {
         if (num < 7) {
             document.getElementById('correct').style.display='block';
         }
+        else if (num == 7) {
+            document.body.style.backgroundColor = "#000000";
+        }
         document.getElementById(`q${num + 1}`).style.display = "block";
     } else {
         document.getElementById('incorrect').style.display='block';
